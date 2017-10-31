42cebf11d72cc914bf0e9749b0ea84d6b878faed
Álvaro Fernando <alvarofernandoms@gmail.com>
2017-10-31 13:11:49 -0200

    Finalizando ConnectionFactory com padrão Module

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,38 +1,57 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function() {
 
-class ConnectionFactory {
-  constructor() {
-    throw new Error('Não é possível criar instâncias de ConnectionFactory')
+  const stores = ['negociacoes'],
+    version = 4,
+    dbName = 'aluraframe';
+
+  let connection = null,
+    close = null;
+
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error('Não é possível criar instâncias de ConnectionFactory')
+    }
+
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        let openRequest = window.indexedDB.open(dbName, version);
+        openRequest.onupgradeneeded = e => {
+          ConnectionFactory._createStores(e.target.result);
+        };
+
+        openRequest.onsuccess = e => {
+          if (!connection) {
+            connection = e.target.result;
+            close = connection.close.bind(connection);
+            connection.close = () => {
+              throw new Error('Você não pode fechar diretamente a conexão');
+            }
+          }
+          resolve(connection);
+        };
+
+        openRequest.onerror = e => {
+          console.log(e.target.error);
+          reject(e.target.error.name);
+        };
+
+      });
+    }
+
+    static _createStores(connection) {
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store))
+          connection.deleteObjectStore(store);
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    }
+
+    static closeConnection() {
+      if (connection) {
+        close();
+        connection = null;
+      }
+    }
+
   }
-
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      let openRequest = window.indexedDB.open(dbName, version);
-      openRequest.onupgradeneeded = e => {
-        ConnectionFactory._createStores(e.target.result);
-      };
-
-      openRequest.onsuccess = e => {
-        resolve(e.target.result);
-      };
-
-      openRequest.onerror = e => {
-        console.log(e.target.error);
-        reject(e.target.error.name);
-      };
-
-    });
-  }
-
-  static _createStores(connection) {
-    stores.forEach(store => {
-      if (connection.objectStoreNames.contains(store))
-        connection.deleteObjectStore(store);
-      connection.createObjectStore(store, { autoIncrement: true });
-    });
-
-  }
-
-}
+})();
