ecd911bfd967cf93a94ca82beff760aa9073c1db
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-01-17 16:03:59 -0200

    Add boas práticas de webpack
    - Otimização com scope hoisting.
    - Separação do código das bibliotecas com o CommonsChunkPlugin.
    - Geração de index.html automaticamente com todos os artefatos produzidos pelo Webpack já importados.
    - Code splitting e Lazy loading.
    - Utilizar System.import() ou import no carregamento de módulos?
    - Onde ficam os arquivos para distribuição do projeto.
    - Como alterar o endereço da API no build de produção com o DefinePlugin.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,28 @@ const babelMinifyPlugin = require('babel-minify-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optmizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
 
+plugins.push(new HtmlWebpackPlugin({
+  hash: true,
+  minify: {
+    html5: true,
+    collapseWhitespace: true,
+    removeComments: true
+  },
+  filename: 'index.html',
+  template: __dirname + '/main.html'
+}))
+
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if (process.env.NODE_ENV == 'production') {
+
+  SERVICE_URL = JSON.stringify('http://endereco-externo-api');
+
+  plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
   plugins.push(new babelMinifyPlugin());
 
   plugins.push(new optmizeCSSAssetsPlugin({
@@ -20,6 +38,8 @@ if (process.env.NODE_ENV == 'production') {
   }));
 }
 
+plugins.push(new webpack.DefinePlugin({ SERVICE_URL }));
+
 plugins.push(new extractTextPlugin('styles.css'));
 
 plugins.push(new webpack.ProvidePlugin({
@@ -27,12 +47,19 @@ plugins.push(new webpack.ProvidePlugin({
   jQuery: 'jquery/dist/jquery'
 }))
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+  name: 'vendor',
+  filename: 'vendor.bundle.js'
+}));
+
 module.exports = {
-  entry: './app-src/app.js',
+  entry: {
+    app: './app-src/app.js',
+    vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: "dist"
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
